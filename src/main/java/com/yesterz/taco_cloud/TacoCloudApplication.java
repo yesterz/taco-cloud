73ce2830f0c9c64698cb53e592dbdc88b82cc728
yesterz <yesterz@163.com>
2024-07-06 09:20:38 +0800

    init

---
+++ src/main/java/com/yesterz/taco_cloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.yesterz.taco_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yesterz/taco_cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesterz.taco_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
